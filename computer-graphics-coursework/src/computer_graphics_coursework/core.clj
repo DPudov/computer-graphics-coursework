abf83e31b6abd0f4f7a7b91740857aedb918704a
Dmitry Pudov <dimapudov99@gmail.com>
2019-08-10 22:00:11 +0300

    Clisk dependency added

--- computer-graphics-coursework/src/computer_graphics_coursework/core.clj
+++ computer-graphics-coursework/src/computer_graphics_coursework/core.clj
@@ -1,6 +1,11 @@
-(ns computer-graphics-coursework.core)
+(ns computer-graphics-coursework.core
+    (:use [clisk live]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def checker-pattern (checker 0 1))
+(defn -main [& args]
+  (show [x y 0])
+  (show (scale 0.25 checker-pattern)))
+  ;(show (viewport [-1 -1] [1 1])
+    ;  (v*
+    ;    (warp globe vplasma)      ;; colour vector plasma texture, samples on the surface of a globe
+    ;    (light-value [-1 -1 1] (height-normal globe))))  ;; diffuse lighting using globe as heightmap
