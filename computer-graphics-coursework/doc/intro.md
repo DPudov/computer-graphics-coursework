330c34200810bbbfa6fc8f29ec8550e1919e68f1
Dmitry Pudov <dimapudov99@gmail.com>
2019-08-10 15:33:22 +0300

    Initial commit

---
+++ computer-graphics-coursework/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to computer-graphics-coursework
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ computer-graphics-coursework/test/computer_graphics_coursework/core_test.clj
@@ -0,0 +1,7 @@
+(ns computer-graphics-coursework.core-test
+  (:require [clojure.test :refer :all]
+            [computer-graphics-coursework.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
