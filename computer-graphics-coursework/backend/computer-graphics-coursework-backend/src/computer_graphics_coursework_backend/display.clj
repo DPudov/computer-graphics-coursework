9cca232784e62729aaf8c624f44edd5ca3214afe
Dmitry Pudov <dimapudov99@gmail.com>
2019-11-29 23:20:27 +0300

    Menu displayed via seesaw

--- computer-graphics-coursework/backend/computer-graphics-coursework-backend/src/computer_graphics_coursework_backend/display.clj
+++ computer-graphics-coursework/backend/computer-graphics-coursework-backend/src/computer_graphics_coursework_backend/display.clj
@@ -1,10 +1,10 @@
 (ns computer_graphics_coursework_backend.display
-  (:import [javax.swing JFrame JLabel JButton JTextField JPanel])
   (:use seesaw.core
         seesaw.graphics
         seesaw.color))
 
 (def title "Курсовая по компьютерной графике. Пудов Д.Ю.")
+(def default-background "#ffffff")
 
 (defn a-sketch [e]
   (println "selecting animation"))
@@ -12,35 +12,82 @@
 (defn a-help [e]
   (println "help..."))
 
-(defn a-about [e]
+(defn a-about-developer [e]
   (println "about..."))
 
+(defn a-about-version [e])
+
+
 (def menus
+  "Define menus at menubar"
   (let [a-sketch (action :handler a-sketch :name "Открыть" :tip "Выбор анимации..." :key "menu N")
         a-help (action :handler a-help :name "Помощь" :tip "Возможные действия в программе" :key "menu H")
-        a-about (action :handler a-about :name "Разработчик" :tip "Информация о разработчике и версии" :key "menu A")]
+        a-about-developer (action :handler a-about-developer :name "Разработчик" :tip "Информация о разработчике" :key "menu A")
+        a-about-version (action :handler a-about-version :name "Версия" :tip "Информация о версии" :key "menu shift V")]
+
     (menubar :items [(menu :text "Анимация..." :items [a-sketch])
                      (menu :text "Помощь" :items [a-help])
-                     (menu :text "О приложении" :items [a-about])])))
+                     (menu :text "О приложении" :items [a-about-developer a-about-version])])))
 
 
 (defn add-menu [frame]
   (config! frame :menubar menus))
 
+
+(defn create-canvas [background]
+  (canvas :id :canvas
+          :background background
+          :paint nil))
+;
+;(defn rotate-controls []
+;  (forms-panel :items ["login" (text) (next-line)
+;                       "Password" (span (text) 3)]))
+;
+;
+(defn create-control-panel []
+  (vertical-panel :items [
+                          (horizontal-panel :items [(vertical-panel :items ["dx" (text)])
+                                                    (vertical-panel :items ["dy" (text)])
+                                                    (vertical-panel :items ["dz" (text)])])
+                          (button :text "Переместить камеру")
+                          (horizontal-panel :items [(vertical-panel :items ["kx" (text)])
+                                                    (vertical-panel :items ["ky" (text)])
+                                                    (vertical-panel :items ["kz" (text)])])
+                          (button :text "Масштабировать")
+                          (horizontal-panel :items [(vertical-panel :items ["ox" (text)])
+                                                    (vertical-panel :items ["oy" (text)])
+                                                    (vertical-panel :items ["oz" (text)])])
+                          (button :text "Повернуть камеру на N градусов")
+                          (horizontal-panel :items [(vertical-panel :items ["x" (text)])
+                                                    (vertical-panel :items ["y" (text)])
+                                                    (vertical-panel :items ["z" (text)])])
+                          (button :text "Изменить координаты источника света")
+                          (horizontal-panel :items [(vertical-panel :items ["x" (text)])
+                                                    (vertical-panel :items ["y" (text)])
+                                                    (vertical-panel :items ["z" (text)])])
+                          (button :text "Изменить координаты наблюдателя")]))
+
+
+(defn create-content []
+  (border-panel :hgap 5 :vgap 5 :border 5
+                :center (create-canvas default-background)
+                :east (create-control-panel)))
+
 (defn create-display-frame
-  ([] (create-display-frame 1300 700))
-  ([width height] (frame :id 1
-                         :title title
-                         :on-close :exit
-                         :width width
-                         :height height
-                         :minimum-size [640 :by 480])))
+  ([content] (create-display-frame content 1300 700))
+  ([content width height] (frame :id :display
+                                 :title title
+                                 :on-close :exit
+                                 :width width
+                                 :height height
+                                 :content content
+                                 :minimum-size [640 :by 480])))
 
 
 
 (defn display-setup [width height]
   (invoke-later
-    (-> (create-display-frame width height)
+    (-> (create-display-frame (create-content) width height)
         add-menu
         show!)))
 
